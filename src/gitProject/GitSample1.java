37bc633864307301aa3f0f160452678f197d8be4
ananthrajk1991 <ananthrajk1991@gmail.com>
2024-10-03 17:32:16 +0530

    code updated

--- src/gitProject/GitSample1.java
+++ src/gitProject/GitSample1.java
@@ -1,6 +1,11 @@
 package gitProject;
 
 public class GitSample1 {
+	
+	private void print() {
+		// TODO Auto-generated method stub
+System.out.println("raj code");
+	}
 	public static void main(String[] args) {
 		System.out.println("git");
 	}
